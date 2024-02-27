e932b4f6d43823cd7d5683fa0a80c6cd604f4f09
Utkarsh Raj <uraj33175@gmail.com>
2024-02-27 14:12:14 +0530

    changed

--- translation.js
+++ translation.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Translate } = require('@google-cloud/translate').v2;
@@ -7,27 +6,42 @@ require('dotenv').config();
 
 app.use(bodyParser.json());
 
-// Initialize Google Cloud Translation API client
-
-
-const port = 3000;
-
-
+// Initialize Translation API client
 const translate = new Translate({
     projectId: process.env.GOOGLE_PROJECT_ID,
     key: process.env.GOOGLE_API_KEY
 });
-app.use(express.json())
+
+const port = 3000;
+
+app.use(express.json());
+
+
+
+// Function to check overflow and underflow
+function checkTextLength(text) {
+    const maxLength = 5000; 
+    const minLength = 1;    
+
+    if (text.length > maxLength) {
+        throw new Error('Text exceeds maximum length limit.');
+    } else if (text.length < minLength) {
+        throw new Error('Text does not meet minimum length requirement.');
+    }
+}
+
 // POST endpoint 
 app.post('/api/translate', async (req, res) => {
     try {
-        // Extract text  from request body
+        // Extract text from request body
         const { text } = req.body;
 
-      
         if (!text) {
             return res.status(400).json({ error: 'Text to translate is required.' });
         }
+        checkTextLength(text);
+
+        
 
         // Translate from English to French
         const [translation] = await translate.translate(text, 'fr');
@@ -38,20 +52,17 @@ app.post('/api/translate', async (req, res) => {
         console.error('Error during translation:', error);
         res.status(500).json({ error: 'An unexpected error occurred.' });
     }
+    
 });
 
 // default route 
 app.use((req, res) => {
-    res.status(404).json({ msg:"Go To Postman url/api/translate" });
+    res.status(404).json({ 
+        error: 'Route not found.',
+        msg:"Go To Postman and Enter site-url/api/translate"
+    });
 });
-
-// Start the server
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
